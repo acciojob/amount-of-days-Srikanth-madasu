84242c24b3ceb252d7dd9e5560db20a5347e08d4
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2023-12-09 21:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(input) {
+	if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log("366");
+		
+		
+    }
+	else{
+		console.log("365")
+
+	}
+	
+}
+const year = prompt('Enter a year:');
